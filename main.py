1ebc0472e5e0dcbb549be0b57ec183e24e8e1519
aanishalien <aanishrizmy28@gmail.com>
2026-08-28 00:53:33 +0530

    feat(phase0):implement webcam capture loop with FPS overlay

---
+++ main.py
@@ -0,0 +1,39 @@
+import time
+import cv2
+
+cap = cv2.VideoCapture(0)
+if not cap.isOpened():
+    raise RuntimeError("Failed to open the webcam stream.")
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+
+print("====================================================")
+print("Webcam capture loop active.")
+print("-> Press [Q] to quit")
+print("====================================================")
+
+prev_time = time.time()
+fps = 0
+
+while cap.isOpened():
+    success, frame = cap.read()
+    if not success:
+        continue
+
+    frame = cv2.flip(frame, 1)
+
+    curr_time = time.time()
+    fps = 1 / (curr_time - prev_time) if curr_time != prev_time else fps
+    prev_time = curr_time
+
+    cv2.putText(frame, f"FPS: {fps:.1f}", (30, 40),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2 ,cv2.LINE_AA)
+    
+    cv2.imshow("Webcam Capture", frame)
+
+    if cv2.waitKey(1) & 0XFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
